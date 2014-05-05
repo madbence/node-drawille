cc29b099cb86a287c3ff482815871a26ecce0306
Bence Dányi <madbence@gmail.com>
2014-05-05 23:48:18 +0200

    Add ridiculously cool clock example

--- example.js
+++ example.js
@@ -1,13 +1,20 @@
 var Canvas = require('./');
+var line = require('bresenham');
 
-var c = new Canvas(160, 20);
+var c = new Canvas(160, 160);
 
-var i = 0;
 function draw() {
-  c.set(i%160, Math.floor(Math.sin(i/5)*10+10));
-  c.unset((i-40+160)%160, Math.floor(Math.sin((i-40)/5)*10+10));
-  i++;
-  setTimeout(draw, 10);
+  c.clear();
+  var t = new Date();
+  var sin = function(i, l) {
+    return Math.floor(Math.sin(i*2*Math.PI)*l+80);
+  }, cos = function(i, l) {
+    return Math.floor(Math.cos(i*2*Math.PI)*l+80);
+  };
+  line(80, 80, sin(t.getHours()/24, 30), 160-cos(t.getHours()/24, 30), c.set.bind(c));
+  line(80, 80, sin(t.getMinutes()/60, 50), 160-cos(t.getMinutes()/60, 50), c.set.bind(c));
+  line(80, 80, sin(t.getSeconds()/60+(+t%1000)/60000, 75), 160-cos(t.getSeconds()/60+(+t%1000)/60000, 75), c.set.bind(c));
+  setTimeout(draw, 1000/60);
   console.log(c.frame());
 }
 
