eb48ee93a0b28e929995306e7323f308699a904a
Bence Dányi <bence@danyi.me>
2017-10-26 11:22:03 +0200

    feat: remove gl-matrix monkeypatch from the cube example

--- examples/cube.js
+++ examples/cube.js
@@ -4,16 +4,6 @@ var mat4 = glmatrix.mat4;
 var vec3 = glmatrix.vec3;
 var Canvas = require('../');
 
-vec3.transformMat4 = function(out, a, m) {
-  var x = a[0], y = a[1], z = a[2],
-    w = m[3] * x + m[7] * y + m[11] * z + m[15];
-  w = w || 1.0;
-  out[0] = (m[0] * x + m[4] * y + m[8] * z + m[12]) / w;
-  out[1] = (m[1] * x + m[5] * y + m[9] * z + m[13]) / w;
-  out[2] = (m[2] * x + m[6] * y + m[10] * z + m[14]) / w;
-  return out;
-};
-
 var c = new Canvas(160, 160);
 
 var points = [[-1,-1,-1],[-1,-1,1],[1,-1,1],[1,-1,-1],[-1,1,-1],[-1,1,1],[1,1,1],[1,1,-1]];
